e6ddb6ce05fbdd4a8f8b219050de42152e052a42
rondon23 <brunorondon21@gmail.com>
2022-06-15 19:26:54 -0400

    fix insert

--- backend/dto/NewProdutoRequest.go
+++ backend/dto/NewProdutoRequest.go
@@ -1,9 +1,16 @@
 package dto
 
-type ProdutoResponse struct {
-	Id           string  `json:"produto_id"`
+import (
+	"pizza-backend/errs"
+)
+
+type NewProdutoRequest struct {
 	NomeProduto  string  `json:"nome_produto"`
 	Descricao    string  `json:"descricao"`
 	ValorProduto float32 `json:"valor_produto"`
 	FotoProduto  string  `json:"foto_produto"`
+}
+
+func (r NewProdutoRequest) Validate() *errs.AppError {
+	return nil
 }
--- backend/dto/NewProdutoResponse.go
+++ backend/dto/NewProdutoResponse.go
@@ -1,6 +1,7 @@
 package dto
 
-type NewProdutoRequest struct {
+type NewProdutoResponse struct {
+	Id           string  `json:"produto_id"`
 	NomeProduto  string  `json:"nome_produto"`
 	Descricao    string  `json:"descricao"`
 	ValorProduto float32 `json:"valor_produto"`
